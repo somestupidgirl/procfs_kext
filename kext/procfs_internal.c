f38f987c772726f4e2948cc6fbf10615a793696f
somestupidgirl <sunnevanattsol@gmail.com>
2022-01-15 23:24:38 +0000

    procfs_internal: some refactoring

--- kext/procfs_internal.c
+++ kext/procfs_internal.c
@@ -1,62 +1,121 @@
 #include "procfs_internal.h"
 
+/*
+ * fp_getfvpandvid
+ *
+ * Description:	Get fileproc, vnode pointer, and vid for a given fd from the
+ *		per process open file table of the specified process, and if
+ *		successful, increment the f_iocount
+ *
+ * Parameters:	p				Process in which fd lives
+ *		fd				fd to get information for
+ *		resultfp			Pointer to result fileproc
+ *						pointer area, or 0 if none
+ *		resultvp			Pointer to result vnode pointer
+ *						area, or 0 if none
+ *		vidp				Pointer to resuld vid area
+ *
+ * Returns:	0				Success
+ *		EBADF				Bad file descriptor
+ *		ENOTSUP				fd does not refer to a vnode
+ *
+ * Implicit returns:
+ *		*resultfp (modified)		Fileproc pointer
+ *		*resultvp (modified)		vnode pointer
+ *		*vidp				vid value
+ *
+ * Notes:	The resultfp and resultvp fields are optional, and may be
+ *		independently specified as NULL to skip returning information
+ *
+ * Locks:	Internally takes and releases proc_fdlock
+ */
+// bsd/kern/kern_descrip.c
 int
-fp_getfsock(struct proc *p, int fd, struct fileproc **resultfp, socket_t *results)
+fp_getfvpandvid(proc_t p, int fd, struct fileproc **resultfp, struct vnode **resultvp, uint32_t *vidp)
 {
-	struct filedesc *fdp = &p->p_fd;
+#if 0
+	struct filedesc *fdp = p->p_fd;
 	struct fileproc *fp;
-	struct fileglob *fg = NULL;
-
-	proc_fdlock(p);
-	if (fd < 0 || fd >= fdp->fd_nfiles ||
-			(fp = fdp->fd_ofiles[fd]) == NULL ||
-			(fdp->fd_ofileflags[fd] & UF_RESERVED)) {
-		proc_fdunlock(p);
-		return (EBADF);
-	}
-	if (&fg->fg_ops->fo_type != DTYPE_SOCKET) {
-		proc_fdunlock(p);
-		return(EOPNOTSUPP);
-	}
-	os_ref_get_count(fp->fp_iocount) <= 1;
-
-	if (resultfp)
-		*resultfp = fp;
-	if (results)
-		*results = (socket_t *)fg->fg_data;
-	proc_fdunlock(p);
-
-	return (0);
-}
-
-int
-fp_getfvpandvid(struct proc *p, int fd, struct fileproc **resultfp,
-		struct vnode **resultvp, uint32_t *vidp)
-{
-	struct filedesc *fdp = &p->p_fd;
-	struct fileproc *fp;
-	struct fileglob *fg = NULL;
 
 	proc_fdlock_spin(p);
 	if (fd < 0 || fd >= fdp->fd_nfiles ||
-			(fp = fdp->fd_ofiles[fd]) == NULL ||
-			(fdp->fd_ofileflags[fd] & UF_RESERVED)) {
+	    (fp = fdp->fd_ofiles[fd]) == NULL ||
+	    (fdp->fd_ofileflags[fd] & UF_RESERVED)) {
 		proc_fdunlock(p);
-		return (EBADF);
+		return EBADF;
 	}
-	if (fg->fg_ops->fo_type != DTYPE_VNODE) {
+	if (fp->f_type != DTYPE_VNODE) {
 		proc_fdunlock(p);
-		return(ENOTSUP);
+		return ENOTSUP;
 	}
-	os_ref_get_count(fp->fp_iocount) <= 1;
+	os_ref_retain_locked(&fp->f_iocount);
 
-	if (resultfp)
+	if (resultfp) {
 		*resultfp = fp;
-	if (resultvp)
-		*resultvp = (struct vnode *)fg->fg_data;
-	if (vidp)
-		*vidp = (uint32_t)vnode_vid((struct vnode *)fg->fg_data);
+	}
+	if (resultvp) {
+		*resultvp = (struct vnode *)fp->f_data;
+	}
+	if (vidp) {
+		*vidp = (uint32_t)vnode_vid((struct vnode *)fp->f_data);
+	}
 	proc_fdunlock(p);
+#endif
+	return 0;
+}
 
-	return (0);
+/*
+ * fp_getfsock
+ *
+ * Description:	Get fileproc and socket pointer for a given fd from the
+ *		per process open file table of the specified process, and if
+ *		successful, increment the f_iocount
+ *
+ * Parameters:	p				Process in which fd lives
+ *		fd				fd to get information for
+ *		resultfp			Pointer to result fileproc
+ *						pointer area, or 0 if none
+ *		results				Pointer to result socket
+ *						pointer area, or 0 if none
+ *
+ * Returns:	EBADF			The file descriptor is invalid
+ *		EOPNOTSUPP		The file descriptor is not a socket
+ *		0			Success
+ *
+ * Implicit returns:
+ *		*resultfp (modified)		Fileproc pointer
+ *		*results (modified)		socket pointer
+ *
+ * Notes:	EOPNOTSUPP should probably be ENOTSOCK; this function is only
+ *		ever called from accept1().
+ */
+// bsd/kern/kern_descrip.c
+int fp_getfsock(proc_t p, int fd, struct fileproc **resultfp, struct socket **results)
+{
+#if 0
+	struct filedesc *fdp = p->p_fd;
+	struct fileproc *fp;
+
+	proc_fdlock_spin(p);
+	if (fd < 0 || fd >= fdp->fd_nfiles ||
+	    (fp = fdp->fd_ofiles[fd]) == NULL ||
+	    (fdp->fd_ofileflags[fd] & UF_RESERVED)) {
+		proc_fdunlock(p);
+		return EBADF;
+	}
+	if (fp->f_type != DTYPE_SOCKET) {
+		proc_fdunlock(p);
+		return EOPNOTSUPP;
+	}
+	os_ref_retain_locked(&fp->f_iocount);
+
+	if (resultfp) {
+		*resultfp = fp;
+	}
+	if (results) {
+		*results = (struct socket *)fp->f_data;
+	}
+	proc_fdunlock(p);
+#endif
+	return 0;
 }
--- kext/procfs_internal.h
+++ kext/procfs_internal.h
@@ -1,112 +1,138 @@
 #ifndef procfs_internal_h
 #define procfs_internal_h
 
+#include <mach/thread_info.h>
 #include <sys/kernel_types.h>
 #include <sys/mount.h>
-
-
-#pragma mark -
-#pragma mark Additional kernel types
-
-struct file_type;
-typedef struct file_type * file_type_t;
-
-struct filedesc;
-typedef struct filedesc * filedesc_t;
-
-struct fileinfo;
-typedef struct fileinfo * fileinfo_t;
-
-struct fileops;
-typedef struct fileops * fileops_t;
-
-struct fileproc;
-typedef struct fileproc * fileproc_t;
-
-struct os_refcnt;
-typedef struct os_refcnt * os_refcnt_t;
-
-struct pgrp;
-typedef struct pgrp * pgrp_t;
-
-struct proc_bsdinfo;
-typedef struct proc_bsdinfo * proc_bsdinfo_t;
-
-struct proc_fileinfo;
-typedef struct proc_fileinfo * proc_fileinfo_t;
-
-struct proc_taskinfo;
-typedef struct proc_taskinfo * proc_taskinfo_t;
-
-struct proc_threadinfo;
-typedef struct proc_threadinfo * proc_threadinfo_t;
-
-struct session;
-typedef struct session * session_t;
-
-struct socket_fdinfo;
-typedef struct socket_fdinfo * socket_fdinfo_t;
-
-struct socket_info;
-typedef struct socket_info * socket_info_t;
-
-struct vnode_fdinfowithpath;
-typedef struct vnode_fdinfowithpath * vnode_fdinfowithpath_t;
-
-struct vnode_info;
-typedef struct vnode_info * vnode_info_t;
-
-struct vnode_info_path;
-typedef struct vnode_info_path * vnode_info_path_t;
+#include <sys/param.h>
+#include <sys/proc.h>
+#include <sys/vnode.h>
 
 
 #pragma mark -
 #pragma mark Macro definitions from XNU
 
 // bsd/sys/filedesc.h
-#define UF_RESERVED        0x04
+#define UF_RESERVED        		0x04
 
 // bsd/sys/mount_internal.h
-#define VFC_VFS64BITREADY   0x100
-#define VFC_VFSNOMACLABEL   0x1000
+#define VFC_VFS64BITREADY   	0x100
+#define VFC_VFSNOMACLABEL   	0x1000
+
+
+#pragma mark -
+#pragma mark Structures
+
+// bsd/sys/proc_info.h
+
+struct proc_bsdinfo {
+	uint32_t                pbi_flags;              /* 64bit; emulated etc */
+	uint32_t                pbi_status;
+	uint32_t                pbi_xstatus;
+	uint32_t                pbi_pid;
+	uint32_t                pbi_ppid;
+	uid_t                   pbi_uid;
+	gid_t                   pbi_gid;
+	uid_t                   pbi_ruid;
+	gid_t                   pbi_rgid;
+	uid_t                   pbi_svuid;
+	gid_t                   pbi_svgid;
+	uint32_t                rfu_1;                  /* reserved */
+	char                    pbi_comm[MAXCOMLEN];
+	char                    pbi_name[2 * MAXCOMLEN];  /* empty if no name is registered */
+	uint32_t                pbi_nfiles;
+	uint32_t                pbi_pgid;
+	uint32_t                pbi_pjobc;
+	uint32_t                e_tdev;                 /* controlling tty dev */
+	uint32_t                e_tpgid;                /* tty process group id */
+	int32_t                 pbi_nice;
+	uint64_t                pbi_start_tvsec;
+	uint64_t                pbi_start_tvusec;
+};
+
+// bsd/sys/proc_info.h
+struct proc_taskinfo {
+	uint64_t                pti_virtual_size;       /* virtual memory size (bytes) */
+	uint64_t                pti_resident_size;      /* resident memory size (bytes) */
+	uint64_t                pti_total_user;         /* total time */
+	uint64_t                pti_total_system;
+	uint64_t                pti_threads_user;       /* existing threads only */
+	uint64_t                pti_threads_system;
+	int32_t                 pti_policy;             /* default policy for new threads */
+	int32_t                 pti_faults;             /* number of page faults */
+	int32_t                 pti_pageins;            /* number of actual pageins */
+	int32_t                 pti_cow_faults;         /* number of copy-on-write faults */
+	int32_t                 pti_messages_sent;      /* number of messages sent */
+	int32_t                 pti_messages_received;  /* number of messages received */
+	int32_t                 pti_syscalls_mach;      /* number of mach system calls */
+	int32_t                 pti_syscalls_unix;      /* number of unix system calls */
+	int32_t                 pti_csw;                /* number of context switches */
+	int32_t                 pti_threadnum;          /* number of threads in the task */
+	int32_t                 pti_numrunning;         /* number of running threads */
+	int32_t                 pti_priority;           /* task priority*/
+};
+
+// bsd/sys/proc_info.h
+struct proc_threadinfo {
+	uint64_t                pth_user_time;          /* user run time */
+	uint64_t                pth_system_time;        /* system run time */
+	int32_t                 pth_cpu_usage;          /* scaled cpu usage percentage */
+	int32_t                 pth_policy;             /* scheduling policy in effect */
+	int32_t                 pth_run_state;          /* run state (see below) */
+	int32_t                 pth_flags;              /* various flags (see below) */
+	int32_t                 pth_sleep_time;         /* number of seconds that thread */
+	int32_t                 pth_curpri;             /* cur priority*/
+	int32_t                 pth_priority;           /*  priority*/
+	int32_t                 pth_maxpriority;        /* max priority*/
+	char                    pth_name[MAXTHREADNAMESIZE];    /* thread name, if any */
+};
+
+// bsd/sys/proc_info.h
+struct proc_fdinfo {
+	int32_t                 proc_fd;
+	uint32_t                proc_fdtype;
+};
+
+struct fileproc;
+struct socket;
+struct socket_info;
 
 
 #pragma mark -
 #pragma mark Function Prototypes for procfs_internal.c
 
-// bsd/sys/proc.h
-// Reimplementation of proc_task() since the function is not available in
-// MacKernelSDK's sys/proc.h due to being guarded by KERNEL_PRIVATE 
-task_t procfs_proc_task(proc_t);
-
 // bsd/sys/file_internal.h
-// Reimplementations of these two functions since they are no longer available in XNU 
-int procfs_fp_getfvpandvid(proc_t *p, int fd, struct fileproc **resultfp, vnode_t **resultvp, uint32_t * vidp);
-int procfs_fp_getfsock(struct proc *p, int fd, struct fileproc **resultfp, struct socket **results);
+// Reimplementations of these two functions since they are no longer available in XNU
+// Merely dummy functions for now, until we figure out how to make them kext-compatible.
+int fp_getfvpandvid(proc_t p, int fd, struct fileproc **resultfp, struct vnode **resultvp, uint32_t *vidp);
+int fp_getfsock(proc_t p, int fd, struct fileproc **resultfp, struct socket **results);
 
 
 #pragma mark -
-#pragma mark TODO FIX: Function definitions from XNU (low priority)
+#pragma mark External function definitions from XNU
 
-int fp_drop(struct proc *p, int fd, struct fileproc *fp, int locked);
+// bsd/sys/file_internal.h
+extern int fp_drop(struct proc *p, int fd, struct fileproc *fp, int locked);
 
+// bsd/kern/proc.h
+extern task_t proc_task(proc_t);
+
+// bsd/kern/proc_info.c
+extern size_t proc_fdlist_internal(proc_t p, struct proc_fdinfo *pfd, size_t numfds);
 extern int proc_pidbsdinfo(proc_t p, struct proc_bsdinfo *pinfo, int zombie);
 extern int proc_pidtaskinfo(proc_t p, struct proc_taskinfo *tinfo);
 extern int proc_pidthreadinfo(proc_t p, uint64_t threadid,  int thuniqueid, struct proc_threadinfo *info);
-
-extern void fill_fileinfo(fileproc_t * fp, proc_t proc, int fd, proc_fileinfo_t * finfo);
+extern void fill_fileinfo(struct fileproc *fp, proc_t proc, int fd, struct proc_fileinfo * finfo);
 extern int fill_socketinfo(socket_t so, struct socket_info *si);
 static int fill_vnodeinfo(vnode_t vp, struct vnode_info *vinfo, boolean_t check_fsgetpath);
 
+// bsd/sys/proc_internal.h
 extern void proc_list_lock(void);
 extern void proc_list_unlock(void);
-
 extern void proc_fdlock(proc_t *);
 extern void proc_fdlock_spin(proc_t *);
 extern void proc_fdunlock(proc_t *);
-
-extern void session_lock(session_t * sess);
-extern void session_unlock(session_t * sess);
-extern struct session * pgrp_session(struct pgrp * pgrp);
+extern void session_lock(struct session * sess);
+extern void session_unlock(struct session * sess);
 
 #endif /* procfs_internal_h */
