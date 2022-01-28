7836b044d23180cfadfade8932715f4520efb912
somestupidgirl <sunnevanattsol@gmail.com>
2022-01-28 14:45:47 +0000

    Revert "procfs_proc.c: Implement proc_iterate(), proc_find_zombref(), proc_drop_zombref() and fix fdlocks"
    This reverts commit 494fb1c1e53f60d9abc515ef144a2eba24818b90.

--- kext/procfs_locks.c
+++ kext/procfs_locks.c
@@ -1,8 +1,6 @@
 #include <kern/locks.h> 
 #include <kern/lock_group.h>
-
 #include <sys/proc.h>
-#include <sys/proc_internal.h>
 
 extern void lck_mtx_lock_spin(lck_mtx_t *lck);
 
@@ -13,6 +11,7 @@ proc_list_lock(void)
     lck_grp_attr_t *proc_lck_grp_attr = lck_grp_attr_alloc_init();
     lck_grp_t *proc_mlock_grp = lck_grp_alloc_init("proc-mlock", proc_lck_grp_attr);
     lck_mtx_t *proc_list_mlock = lck_mtx_alloc_init(proc_mlock_grp, proc_lck_attr);
+
     lck_mtx_lock(proc_list_mlock);
 } 
 
@@ -23,23 +22,39 @@ proc_list_unlock(void)
     lck_grp_attr_t *proc_lck_grp_attr = lck_grp_attr_alloc_init();
     lck_grp_t *proc_mlock_grp = lck_grp_alloc_init("proc-mlock", proc_lck_grp_attr);
     lck_mtx_t *proc_list_mlock = lck_mtx_alloc_init(proc_mlock_grp, proc_lck_attr);
+
     lck_mtx_unlock(proc_list_mlock);
 } 
 
 void
 proc_fdlock(proc_t p)
 {
-    lck_mtx_lock(&p->p_fdmlock);
+    lck_attr_t *proc_lck_attr = lck_attr_alloc_init();
+    lck_grp_attr_t *proc_lck_grp_attr = lck_grp_attr_alloc_init();
+    lck_grp_t *proc_fdmlock_grp = lck_grp_alloc_init("proc-fdmlock", proc_lck_grp_attr);
+    lck_mtx_t *proc_list_fdmlock = lck_mtx_alloc_init(proc_fdmlock_grp, proc_lck_attr);
+
+    lck_mtx_lock(proc_list_fdmlock);
 } 
 
 void
 proc_fdlock_spin(proc_t p)
 {
-    lck_mtx_lock_spin(&p->p_fdmlock);
+   lck_attr_t *proc_lck_attr = lck_attr_alloc_init();
+   lck_grp_attr_t *proc_lck_grp_attr = lck_grp_attr_alloc_init();
+   lck_grp_t *proc_fdmlock_grp = lck_grp_alloc_init("proc-fdmlock", proc_lck_grp_attr);
+   lck_mtx_t *proc_list_fdmlock = lck_mtx_alloc_init(proc_fdmlock_grp, proc_lck_attr);
+
+   lck_mtx_lock_spin(proc_list_fdmlock);
 }
 
 void
 proc_fdunlock(proc_t p)
 {
-    lck_mtx_unlock(&p->p_fdmlock);
-}
+    lck_attr_t *proc_lck_attr = lck_attr_alloc_init();
+    lck_grp_attr_t *proc_lck_grp_attr = lck_grp_attr_alloc_init();
+    lck_grp_t *proc_fdmlock_grp = lck_grp_alloc_init("proc-fdmlock", proc_lck_grp_attr);
+    lck_mtx_t *proc_list_fdmlock = lck_mtx_alloc_init(proc_fdmlock_grp, proc_lck_attr);
+
+    lck_mtx_unlock(proc_list_fdmlock);
+} 
