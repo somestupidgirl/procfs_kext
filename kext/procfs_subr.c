ff8ada285813407de35a18f57b9ac6814239921e
somestupidgirl <sunnevanattsol@gmail.com>
2022-02-08 23:11:36 +0000

    procfs_get_process_count(): kauth_cred_issuser does nothing but 'return kauth_cred_getuid(creds) == 0' so replace it with that

--- kext/procfs_subr.c
+++ kext/procfs_subr.c
@@ -37,7 +37,6 @@ static kern_return_t (*_proc_iterate)(unsigned int flags, proc_iterate_fn_t call
 static kern_return_t (*_task_threads)(task_t task, thread_act_array_t *threads_out, mach_msg_type_number_t *count);
 static kern_return_t (*_thread_info)(thread_t thread, thread_flavor_t flavor, thread_info_t thread_info, mach_msg_type_number_t *thread_info_count);
 static thread_t (*_convert_port_to_thread)(ipc_port_t port);
-static int (*_kauth_cred_issuser)(kauth_cred_t _cred);
 
 #pragma mark -
 #pragma mark Function Prototypes.
@@ -204,9 +203,7 @@ procfs_get_process_count(kauth_cred_t creds) {
     int process_count;
     uint32_t size;
 
-    _kauth_cred_issuser = (void*)lookup_symbol("_kauth_cred_issuser");
-
-    boolean_t is_suser = _kauth_cred_issuser(creds) == 0;
+    boolean_t is_suser = kauth_cred_getuid(creds) == 0;
     procfs_get_pids(&pidp, &process_count, &size, is_suser ? NULL : creds);
     procfs_release_pids(pidp, size);
     
