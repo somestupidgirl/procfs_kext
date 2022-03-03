27ad33afbe01fe77c3d935a5b8211c74fad827a8
somestupidgirl <sunnevanattsol@gmail.com>
2022-03-03 16:37:19 +0000

    kext: read_thread_info(): FIXME

--- kext/procfs_data.c
+++ kext/procfs_data.c
@@ -242,6 +242,7 @@ procfs_read_thread_info(procfsnode_t *pnp, uio_t uio, __unused vfs_context_t ctx
     // Get the process id and thread from the node id in the procfsnode and locate
     // the process.
     int error = 0;
+#if 0
     proc_t p = proc_find(pnp->node_id.nodeid_pid);
     if (p != NULL) {
         struct proc_threadinfo info;
@@ -257,6 +258,7 @@ procfs_read_thread_info(procfsnode_t *pnp, uio_t uio, __unused vfs_context_t ctx
             p = NULL;
         }
     }
+#endif
     return error;
 }
 
