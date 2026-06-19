93747f01379f149fef54e87909a0d5d1ded35143
somestupidgirl <sunnevanattsol@gmail.com>
2026-06-19 07:28:06 +0000

    xnu: bsd: Update sys/tty,h

--- xnu/bsd/sys/tty.h
+++ xnu/bsd/sys/tty.h
@@ -73,6 +73,11 @@
 #include <sys/cdefs.h>
 #include <sys/termios.h>
 #include <sys/select.h>         /* For struct selinfo. */
+//#if XNU_KERNEL_PRIVATE
+#include <os/refcnt.h>
+//#endif
+
+//#ifdef KERNEL
 
 __BEGIN_DECLS
 #include <kern/locks.h>
@@ -108,74 +113,45 @@ struct clist {
  * (low, high, timeout).
  */
 struct tty {
-	lck_mtx_t          t_lock;                                     /* Per tty lock */
-	struct             clist t_rawq;                               /* Device raw input queue. */
-	long               t_rawcc;                                    /* Raw input queue statistics. */
-	struct clist       t_canq;                                     /* Device canonical queue. */
-	long               t_cancc;                                    /* Canonical queue statistics. */
-	struct clist       t_outq;                                     /* Device output queue. */
-	long               t_outcc;                                    /* Output queue statistics. */
-	int                t_line;                                     /* Interface to device drivers. */
-	dev_t              t_dev;                                      /* Device. */
-	int                t_state;                                    /* Device and driver (TS*) state. */
-	int                t_flags;                                    /* Tty flags. */
-	int                t_timeout;                                  /* Timeout for ttywait() */
-	struct pgrp       *t_pgrp;                                     /* Foreground process group. */
-	struct session    *t_session;                                  /* Enclosing session. */
-	struct selinfo     t_rsel;                                     /* Tty read/oob select. */
-	struct selinfo     t_wsel;                                     /* Tty write select. */
-	struct termios     t_termios;                                  /* Termios state. */
-	struct winsize     t_winsize;                                  /* Window size. */
-	void             (*t_oproc)(struct tty *);                     /* Start output. */
-	void             (*t_stop)(struct tty *, int);                 /* Stop output. */
-	int              (*t_param)(struct tty *, struct termios *);   /* Set hardware state. */
-	void              *t_sc;                                       /* XXX: net/if_sl.c:sl_softc. */
-	int                t_column;                                   /* Tty output column. */
-	int                t_rocount, t_rocol;                         /* See explanation below */
-	int                t_hiwat;                                    /* High water mark. */
-	int                t_lowat;                                    /* Low water mark. */
-	int                t_gen;                                      /* Generation number. */
-	void              *t_iokit;                                    /* IOKit management */
-	int                t_refcnt;                                   /* reference count */
-};
+	lck_mtx_t       t_lock;         /* Per tty lock */
 
-/*
- * Explanation of t_rocol and t_rocount:
- * In canonical mode, the line that the user is typing can be echoed back
- * to the serial device, so that the user knows what they're typing.
- * However, processes can still write to the terminal, so the echoed line
- * can be interleaved with output from processes.
- * When the user wants to erase characters from the line, we only want to
- * rub out (i.e. make no longer visible) characters that the user has typed,
- * but we must preserve characters output by processes.
- * For example, suppose the user types 'foo', a process outputs 'qux', and
- * then the user types 'bar'. The line on the display device should look like
- * this:
- * fooquxbar
- * Note that the contents of the line buffer are 'foobar'.
- * We can safely erase 'bar' from the line, since it's uninterrupted by output
- * from processes. After the user presses the erase key 3 times, the line on the
- * display device will look like this:
- * fooqux
- * Now, what should happen when the user presses the erase key? We must preserve
- * the 'qux' output by the process, since that's not recoverable. We therefore
- * re-echo the contents of the line buffer (with the last character removed) to
- * the screen:
- * fooqux
- * fo
- * Clearly, we need to be able to tell when it's safe to rub out the last
- * character in the line buffer. More precisely, we need to know whether the
- * last character on the line displayed to the user comes from the user
- * or from a process. We therefore keep track of the longest suffix
- * of the line buffer that was echoed to the screen uninterrupted.
- * The length of that suffix is stored in t_rocount, and the column index
- * of the first character in the suffix is stored in t_rocol.
- * When a character typed by the user is echoed, we increment t_rocount.
- * When a process outputs any character, t_rocount is set to 0.
- * Erasing a character decrements t_rocount.
- * When t_rocount > 0, we know we can safely erase the last character
- * in the line buffer. When t_rocount == 0, we must re-echo the line buffer.
- */
+	struct  clist t_rawq;           /* Device raw input queue. */
+	long    t_rawcc;                /* Raw input queue statistics. */
+	struct  clist t_canq;           /* Device canonical queue. */
+	long    t_cancc;                /* Canonical queue statistics. */
+	struct  clist t_outq;           /* Device output queue. */
+	long    t_outcc;                /* Output queue statistics. */
+	int     t_line;                 /* Interface to device drivers. */
+	dev_t   t_dev;                  /* Device. */
+	int     t_state;                /* Device and driver (TS*) state. */
+	int     t_flags;                /* Tty flags. */
+	int     t_timeout;              /* Timeout for ttywait() */
+	struct  pgrp *t_pgrp;           /* (TTYL+LL) Foreground process group. */
+	struct  session *t_session;     /* (TTYL+LL) Enclosing session. */
+	struct  selinfo t_rsel;         /* Tty read/oob select. */
+	struct  selinfo t_wsel;         /* Tty write select. */
+	struct  termios t_termios;      /* Termios state. */
+	struct  winsize t_winsize;      /* Window size. */
+	                                /* Start output. */
+	void    (*t_oproc)(struct tty *);
+	/* Stop output. */
+	void    (*t_stop)(struct tty *, int);
+	/* Set hardware state. */
+	int     (*t_param)(struct tty *, struct termios *);
+	void    *t_sc;                  /* XXX: net/if_sl.c:sl_softc. */
+	int     t_column;               /* Tty output column. */
+	int     t_rocount, t_rocol;     /* Tty. */
+	int     t_hiwat;                /* High water mark. */
+	int     t_lowat;                /* Low water mark. */
+	int     t_gen;                  /* Generation number. */
+	void    *t_iokit;               /* IOKit management */
+//#if XNU_KERNEL_PRIVATE
+	os_ref_atomic_t t_refcnt;
+//#else
+//	int     t_refcnt;               /* reference count */
+//#endif
+	thread_t t_locked_thread;       /* thread that owns the lock */
+};
 
 #define TTY_NULL (struct tty *)NULL
 
@@ -190,29 +166,31 @@ struct tty {
 #define t_time          t_termios.c_time
 
 
-#define TTIPRI          25              /* Sleep priority for tty reads. */
-#define TTOPRI          26              /* Sleep priority for tty writes. */
+#define TTIPRI  25                      /* Sleep priority for tty reads. */
+#define TTOPRI  26                      /* Sleep priority for tty writes. */
 
 /*
  * User data unfortunately has to be copied through buffers on the way to
  * and from clists.  The buffers are on the stack so their sizes must be
  * fairly small.
  */
-#define IBUFSIZ         384             /* Should be >= max value of MIN. */
-#define OBUFSIZ         100
+#define IBUFSIZ 384                     /* Should be >= max value of MIN. */
+#define OBUFSIZ 100
 
 #ifndef TTYHOG
-#define TTYHOG          1024
+#define TTYHOG  1024
 #endif
 
 #define TTMAXHIWAT      roundup(2048, CBSIZE)
 #define TTMINHIWAT      roundup(100, CBSIZE)
 #define TTMAXLOWAT      256
 #define TTMINLOWAT      32
+//#else
+//struct tty;
+//struct clist;
+//#endif /* KERNEL */
 
-/*
- * These flags are kept in t_state.
- */
+/* These flags are kept in t_state. */
 #define TS_SO_OLOWAT    0x00001         /* Wake up when output <= low water. */
 #define TS_ASYNC        0x00002         /* Tty in async I/O mode. */
 #define TS_BUSY         0x00004         /* Draining output. */
@@ -227,9 +205,7 @@ struct tty {
 #endif
 #define TS_XCLUDE       0x00400         /* Tty requires exclusivity. */
 
-/*
- * State for intra-line fancy editing work.
- */
+/* State for intra-line fancy editing work. */
 #define TS_BKSL         0x00800         /* State for lowercase \ work. */
 #define TS_CNTTB        0x01000         /* Counting tab width, ignore FLUSHO. */
 #define TS_ERASE        0x02000         /* Within a \.../ for PRTRUB. */
@@ -237,18 +213,14 @@ struct tty {
 #define TS_TYPEN        0x08000         /* Retyping suspended input (PENDIN). */
 #define TS_LOCAL        (TS_BKSL | TS_CNTTB | TS_ERASE | TS_LNCH | TS_TYPEN)
 
-/*
- * Extras.
- */
+/* Extras. */
 #define TS_CAN_BYPASS_L_RINT 0x010000   /* Device in "raw" mode. */
 #define TS_CONNECTED    0x020000        /* Connection open. */
 #define TS_SNOOP        0x040000        /* Device is being snooped on. */
 #define TS_SO_OCOMPLETE 0x080000        /* Wake up when output completes. */
 #define TS_ZOMBIE       0x100000        /* Connection lost. */
 
-/*
- * Hardware flow-control-invoked bits.
- */
+/* Hardware flow-control-invoked bits. */
 #define TS_CAR_OFLOW    0x200000        /* For MDMBUF (XXX handle in driver). */
 #ifdef notyet
 #define TS_CTS_OFLOW    0x400000        /* For CCTS_OFLOW. */
@@ -258,9 +230,8 @@ struct tty {
 #define TS_IOCTL_NOT_OK 0x1000000       /* Workaround <rdar://....> */
 #define TS_REVOKE       0x2000000       /* Terminal getting revoked */
 
-/*
- * Character type information.
- */
+
+/* Character type information. */
 #define ORDINARY        0
 #define CONTROL         1
 #define BACKSPACE       2
@@ -274,9 +245,7 @@ struct speedtab {
 	int sp_code;
 };
 
-/*
- * Modem control commands (driver).
- */
+/* Modem control commands (driver). */
 #define DMSET           0
 #define DMBIS           1
 #define DMBIC           2
@@ -291,6 +260,8 @@ struct speedtab {
 #define TTY_OE          0x04000000      /* Overrun error */
 #define TTY_BI          0x08000000      /* Break condition */
 
+//#ifdef KERNEL
+
 /* Unique sleep addresses. */
 #define TSA_CARR_ON(tp)         ((void *)&(tp)->t_rawq)
 #define TSA_HUP_OR_INPUT(tp)    ((void *)&(tp)->t_rawq.c_cf)
@@ -303,72 +274,82 @@ struct speedtab {
 
 __BEGIN_DECLS
 
+int      b_to_q(const u_char *cp, int cc, struct clist *q);
+void     catq(struct clist *from, struct clist *to);
+void     clist_init(void);
+int      getc(struct clist *q);
+void     ndflush(struct clist *q, int cc);
+int      ndqb(struct clist *q, int flag);
+u_char  *firstc(struct clist *clp, int *c);
+u_char  *nextc(struct clist *q, u_char *cp, int *c);
+int      putc(int c, struct clist *q);
+int      q_to_b(struct clist *q, u_char *cp, int cc);
+int      unputc(struct clist *q);
+int      clalloc(struct clist *clp, int size, int quot);
+void     clfree(struct clist *clp);
+void    cinit(void);
+void    clrbits(u_char *cp, int off, int len);
+
+//#ifdef KERNEL_PRIVATE
+void    tty_init(void);
 /*
- * These functions are not explicitly guarded
- * by the KERNEL_PRIVATE or XNU_KERNEL_PRIVATE
- * definitions so whether they can be used is
- * currently unknown. Commented out for now.
+ * The locked version of this function is used from routines which hold
+ * the tty_lock(), such as ttcompat() in tty_compat.c
  */
-#if 0
-int         b_to_q(const u_char *cp, int cc, struct clist *q);
-void        catq(struct clist *from, struct clist *to);
-void        clist_init(void);
-int         getc(struct clist *q);
-void        ndflush(struct clist *q, int cc);
-int         ndqb(struct clist *q, int flag);
-u_char     *firstc(struct clist *clp, int *c);
-u_char     *nextc(struct clist *q, u_char *cp, int *c);
-int         putc(int c, struct clist *q);
-int         q_to_b(struct clist *q, u_char *cp, int cc);
-int         unputc(struct clist *q);
-int         clalloc(struct clist *clp, int size, int quot);
-void        clfree(struct clist *clp);
-void        cinit(void);
-void        clrbits(u_char *cp, int off, int len);
+int     ttioctl_locked(struct tty *tp, u_long com, caddr_t data, int flag,
+    struct proc *p);
 
-void        tty_lock(struct tty *tp);
-void        tty_unlock(struct tty *tp);
+int     ttcompat(struct tty *tp, u_long com, caddr_t data, int flag,
+    struct proc *p);
+//#endif /* KERNEL_PRIVATE */
 
-void        termioschars(struct termios *t);
-int         tputchar(int c, struct tty *tp);
-int         ttioctl(struct tty *tp, u_long com, caddr_t data, int flag, struct proc *p);
-int         ttread(struct tty *tp, struct uio *uio, int flag);
-int         ttyselect(struct tty *tp, int rw, void * wql, struct proc *p);
-int         ttselect(dev_t dev, int rw, void * wql, struct proc *p);
-void        ttsetwater(struct tty *tp);
-int         ttspeedtab(int speed, struct speedtab *table);
-int         ttstart(struct tty *tp);
-void        ttwakeup(struct tty *tp);
-int         ttwrite(struct tty *tp, struct uio *uio, int flag);
-void        ttwwakeup(struct tty *tp);
-void        ttyblock(struct tty *tp);
-int         ttycheckoutq(struct tty *tp, int wait);
-int         ttyclose(struct tty *tp);      /* LEGACY: avoid using */
-void        ttyflush(struct tty *tp, int rw);
-void        ttyinfo(struct tty *tp);
-void        ttyinfo_locked(struct tty *tp);
-int         ttyinput(int c, struct tty *tp);
-int         ttylclose(struct tty *tp, int flag);
-int         ttymodem(struct tty *tp, int flag);
-int         ttyopen(dev_t device, struct tty *tp);
-int         ttysleep(struct tty *tp, void *chan, int pri, const char *wmesg, int timeout);
-int         ttywait(struct tty *tp);
+void tty_lock(struct tty *tp);
+bool tty_trylock(struct tty *tp);
+void tty_unlock(struct tty *tp);
+bool tty_islocked(struct tty *tp);
+
+void     termioschars(struct termios *t);
+int      tputchar(int c, struct tty *tp);
+int      ttioctl(struct tty *tp, u_long com, caddr_t data, int flag,
+    struct proc *p);
+int      ttread(struct tty *tp, struct uio *uio, int flag);
+int      ttyselect(struct tty *tp, int rw, void * wql, struct proc *p);
+int      ttselect(dev_t dev, int rw, void * wql, struct proc *p);
+void     ttsetwater(struct tty *tp);
+int      ttspeedtab(int speed, struct speedtab *table);
+int      ttstart(struct tty *tp);
+void     ttwakeup(struct tty *tp);
+int      ttwrite(struct tty *tp, struct uio *uio, int flag);
+void     ttwwakeup(struct tty *tp);
+void     ttyblock(struct tty *tp);
+int      ttycheckoutq(struct tty *tp, int wait);
+int      ttyclose(struct tty *tp);      /* LEGACY: avoid using */
+void     ttyflush(struct tty *tp, int rw);
+void     ttyinfo(struct tty *tp);
+void     ttyinfo_locked(struct tty *tp);
+int      ttyinput(int c, struct tty *tp);
+int      ttylclose(struct tty *tp, int flag);
+int      ttymodem(struct tty *tp, int flag);
+int      ttyopen(dev_t device, struct tty *tp);
+int      ttysleep(struct tty *tp,
+    void *chan, int pri, const char *wmesg, int timeout);
+int      ttywait(struct tty *tp);
 struct tty *ttymalloc(void);
-void        ttyfree(struct tty *);
-#endif
+void     ttyfree(struct tty *);
+void     ttyfree_locked(struct tty *);
+
+//#ifdef XNU_KERNEL_PRIVATE
+extern void ttyhold(struct tty *tp);
 
 #define TTY_LOCK_OWNED(tp) LCK_MTX_ASSERT(&tp->t_lock, LCK_MTX_ASSERT_OWNED)
 #define TTY_LOCK_NOTOWNED(tp) LCK_MTX_ASSERT(&tp->t_lock, LCK_MTX_ASSERT_NOTOWNED)
 
 #define PTS_MAJOR 4
 #define PTC_MAJOR 5
-
-/*
- * If you need accounting consider using
- * KALLOC_HEAP_DEFINE to define a view.
- */
-#define KM_TTYS     KHEAP_DEFAULT
+//#endif /* defined(XNU_KERNEL_PRIVATE) */
 
 __END_DECLS
+
+//#endif /* KERNEL */
 
 #endif /* !_SYS_TTY_H_ */
